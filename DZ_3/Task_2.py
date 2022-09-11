1aca000b40d154aa9ecd97597977596e7415aedc
RomanBusygin <roman.alex.95@mail.ru>
2022-09-11 09:21:42 +0500

    Task 2

--- DZ_3/Task_2.py
+++ DZ_3/Task_2.py
@@ -3,14 +3,9 @@ import math
 
 def couple_multiply(multiply_list: list):
     multiply = 1
-    if len(multiply_list) % 2 == 0:
-        for i in range(math.ceil(len(multiply_list) / 2)):
-            multiply = int(multiply_list[i]) * int(multiply_list[-i - 1])
-            print(multiply, end= ' ')
-    else:
-        for i in range(math.ceil(len(multiply_list) / 2)):
-            multiply = int(multiply_list[i]) * int(multiply_list[-i - 1])
-            print(multiply, end= ' ')
+    for i in range(math.ceil(len(multiply_list) / 2)):
+        multiply = int(multiply_list[i]) * int(multiply_list[-i - 1])
+        print(multiply, end= ' ')
 
 number_list = input('Введите числа через пробел: ').split(' ')
 couple_multiply(number_list)
