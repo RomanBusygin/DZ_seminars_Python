9b05c185a9c9ddcf9bc31a55bdc229d1092e2374
RomanBusygin <roman.alex.95@mail.ru>
2022-09-11 20:50:05 +0500

    Task 4

--- DZ_4/Task_4.py
+++ DZ_4/Task_4.py
@@ -5,11 +5,17 @@ def gen_polinom(degree: int) -> str:
     polinom = ""
     for i in range(k, -1, -1):
         if i > 1:
-            polinom += f'{randint(0, 100)}x^{i} + '
+            rand = randint(0, 100)
+            if rand != 0:
+                polinom += f'{rand}x^{i} + '
         elif i == 1:
-            polinom += f'{randint(0, 100)}x + '
+            rand = randint(0, 100)
+            if rand != 0:
+                polinom += f'{rand}x + '
         elif i == 0:
-            polinom += f'{randint(0, 100)}'
+            rand = randint(0, 100)
+            if rand != 0:
+                polinom += f'{rand}'
     return polinom
 
 k = int(input('Введите натуральную степень k: '))
